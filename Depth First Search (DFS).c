376beabeae7f451d43affebfed81a04cbd26243d
Abhay gupta <abhaygpta12@gmail.com>
2026-04-04 15:39:19 +0530

    Add files via upload

---
+++ Depth First Search (DFS).c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int visited[MAX];
+
+void dfs(int v, int n, int adj[MAX][MAX]) {
+    visited[v] = 1;
+    printf("%d ", v);
+
+    for (int i = 0; i < n; i++) {
+        if (adj[v][i] == 1 && visited[i] == 0) {
+            dfs(i, n, adj);
+        }
+    }
+}
+
+int main() {
+    int n, s;
+    int adj[MAX][MAX];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &s);
+
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    printf("DFS Traversal: ");
+    dfs(s, n, adj);
+
+    return 0;
+}
